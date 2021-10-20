d1bf93cb303d7e9aa3c02b75ac693bbb287bff2e
Celine Salzmann <celine@smartive.ch>
2021-10-20 17:41:04 +0200

    prop types

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,9 +9,11 @@ export const Footer: FC = () => {
         <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
           {navigation.main.map((item) => (
             <div key={item.name} className="px-5 py-2">
-              <a href={item.href} className="text-sm text-gray-500 hover:text-gray-900">
-                {item.name}
-              </a>
+              <Link href={item.href}>
+                <a target={item.newTab ? '_blank' : ''} className="text-sm text-gray-500 hover:text-gray-900">
+                  {item.name}
+                </a>
+              </Link>
             </div>
           ))}
         </nav>
--- src/data/navigation.tsx
+++ src/data/navigation.tsx
@@ -1,15 +1,17 @@
+import { SVGProps } from 'react';
+
 export const navigation = {
   main: [
     { name: 'Theorie', href: '/theorie', newTab: false },
-    { name: 'Abschlussarbeit', href: '#', newTab: true },
-    { name: 'smartive™', href: 'https://smartive.ch', target: '_blank' },
+    { name: 'Abschlussarbeit', href: '/#', newTab: false },
+    { name: 'smartive™', href: 'https://smartive.ch', newTab: true },
   ],
   social: [
     {
       name: 'Facebook',
       href: 'https://smartive.us1.list-manage.com/track/click?u=b7783121353e2508ff91af015&id=0ddfa7f812&e=71f00677d1',
-      newTab: false,
-      icon: (props) => (
+      newTab: true,
+      icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -22,9 +24,9 @@ export const navigation = {
     {
       name: 'Instagram',
       href: 'https://smartive.us1.list-manage.com/track/click?u=b7783121353e2508ff91af015&id=ce0be6a454&e=71f00677d1',
-      newTab: false,
+      newTab: true,
 
-      icon: (props) => (
+      icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path
             fillRule="evenodd"
@@ -37,9 +39,9 @@ export const navigation = {
     {
       name: 'Twitter',
       href: 'https://smartive.us1.list-manage.com/track/click?u=b7783121353e2508ff91af015&id=080aea21c2&e=71f00677d1',
-      newTab: false,
+      newTab: true,
 
-      icon: (props) => (
+      icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
           <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
         </svg>
@@ -48,9 +50,8 @@ export const navigation = {
     {
       name: 'LinkedIn',
       href: 'https://smartive.us1.list-manage.com/track/click?u=b7783121353e2508ff91af015&id=a15f2b5d28&e=71f00677d1',
-      newTab: false,
-
-      icon: (props) => (
+      newTab: true,
+      icon: () => (
         <svg fill="currentColor" width="24" height="24" viewBox="0 0 24 24">
           <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
         </svg>
