e7d5db2e781269c973a120dc220521f0e0510bf8
Celine Salzmann <celine@smartive.ch>
2021-10-21 15:50:29 +0200

    some adaptations

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link';
 export const Footer: FC = () => {
   return (
     <footer>
-      <div className="max-w-7xl mx-auto py-16 px-4 overflow-hidden sm:px-6 lg:px-8">
-        <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
+      <div className="max-w-7xl mx-auto lg:py-16 px-4 sm:px-6">
+        <nav className="flex flex-wrap justify-center px-24" aria-label="Footer">
           {navigation.main.map((item) => (
             <div key={item.name} className="px-5 py-2">
               <Link href={item.href}>
@@ -27,7 +27,9 @@ export const Footer: FC = () => {
             </Link>
           ))}
         </div>
-        <p className="mt-8 text-center text-s text-gray-400">&copy; 2022 Bachelorarbeit UZH – smartive™.</p>
+        <p className="mt-8 text-center text-xxs lg:text-s md:text-s text-gray-400">
+          &copy; 2022 Bachelorarbeit UZH – smartive™.
+        </p>
       </div>
     </footer>
   );
--- src/layouts/page.tsx
+++ src/layouts/page.tsx
@@ -5,18 +5,18 @@ import { Footer } from '../components/footer';
 
 export const Page: FC = ({ children }) => {
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-100 ">
       <header className="font-sans font-bold text-xs py-4">
-        <div className="max-h-[1px] py-[4px] mx-auto">
+        <div className="text-center">
           <Link href="https://www.smartive.ch">
-            <a target="_blank" rel="noreferrer">
+            <a target="_blank" rel="noreferrer" className="inline-block">
               <Logo />
             </a>
           </Link>
         </div>
       </header>
 
-      <main className="lg:container lg:mx-auto px-24 py-16 border-b-2">{children}</main>
+      <main className="lg:container lg:mx-auto px-8 lg:px-24 py-16 border-b-2">{children}</main>
       <Footer></Footer>
     </div>
   );
