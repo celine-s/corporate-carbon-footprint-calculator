e943b68d30f2585a79ddd9b7b038cf78c2013465
Celine Salzmann <celine@smartive.ch>
2021-11-24 14:23:44 +0100

    onClick on link

--- src/elements/link.tsx
+++ src/elements/link.tsx
@@ -5,14 +5,16 @@ export type LinkProps = {
   newTab?: boolean;
   href: string;
   border?: boolean;
+  onClick?: () => void;
 };
 
-export const LinkElement: FC<LinkProps> = ({ newTab = false, children, href, border = false }) => {
+export const LinkElement: FC<LinkProps> = ({ newTab = false, children, href, border = false, onClick }) => {
   const borderStyle = border ? 'border-b-2 hover:border-black' : '';
   return (
     <Link href={href}>
       <a
         target={newTab ? '_blank' : ''}
+        onClick={onClick}
         className={`inline-block text-center text-xs md:text-sm text-gray-500 hover:text-gray-900 ${borderStyle}`}
       >
         {children}
