f5a7da8c1b236ef2b4f33ab7d9b8adc8c2636594
Celine Salzmann <celine@smartive.ch>
2021-12-15 15:14:18 +0100

    fortschrittsanzeige und refactoring adaptations

--- src/pages/rechner/[frage].tsx
+++ src/pages/rechner/[frage].tsx
@@ -7,17 +7,15 @@ import { getLocalStorage, setLocalStorage } from '../../utils/local-storage';
 import { LinkElement } from '../../elements/link';
 import { useRouter } from 'next/dist/client/router';
 import { CategoriesNavigation } from '../../compositions/categories-navigation';
-import { initializeFirestore } from '../../utils/get-firestore';
-import { collection, doc, getDocs, query, setDoc } from 'firebase/firestore/lite';
 import { Question } from '../../data/question';
 import { Button } from '../../elements/button';
-import Questions from '../../data/questions.json';
 import { InformationCircleIcon } from '@heroicons/react/solid';
+import { getQuestions } from '../../utils/questions-helper';
 
 type Props = {
   question: Question;
   MAX_QUESTION_NUMBER: number;
-  allCategoriesWithIndexes: { [key: string]: string[] };
+  categoriesWithIndexes?: { [key: string]: string[] };
 };
 
 const LOCALSTORAGE_IMPACT_KEY = 'impact';
@@ -33,7 +31,7 @@ const Frage: NextPage<Props> = ({
   question,
   question: { id, title, label, emissionfactor, initialAnswer, category },
   MAX_QUESTION_NUMBER,
-  allCategoriesWithIndexes,
+  categoriesWithIndexes,
 }) => {
   const [answer, setAnswer] = useState(getLocalStorage(id) || '');
   const [impact, setImpact] = useState(getLocalStorage(LOCALSTORAGE_IMPACT_KEY + id) || '');
@@ -51,67 +49,63 @@ const Frage: NextPage<Props> = ({
     setLocalStorage(LOCALSTORAGE_IMPACT_KEY + id, impact);
   };
 
+  const ID = 'w';
   const hrefNextQuestion =
-    parseInt(id) >= MAX_QUESTION_NUMBER ? '/rechner/report' : `/rechner/${(parseInt(id) + 1).toString()}`;
+    parseInt(id) >= MAX_QUESTION_NUMBER ? `/rechner/report/${ID}` : `/rechner/${(parseInt(id) + 1).toString()}`;
 
-  const categoryLength = allCategoriesWithIndexes?.[category].length;
-  const questionWeAre = (allCategoriesWithIndexes?.[category].findIndex((element) => element === id) + 1).toString();
-  const grayBar =
-    allCategoriesWithIndexes?.[category].findIndex((element) => element === id) + 1 === categoryLength ? 'hidden' : 'grid';
   return (
-    <CategoriesNavigation question={question} categoriesWithIndexes={allCategoriesWithIndexes}>
-      <div className={`grid grid-cols-${categoryLength}`}>
-        <div className={`grid col-span-${questionWeAre} border-cornflower-500 border-4 my-6`} />
-        <div className={`${grayBar} border-gray-200 border-4 my-6`} />
-      </div>
-      <div className="text-xs py-2">
-        Frage {id}/{MAX_QUESTION_NUMBER}
-      </div>
-      <div>
-        <div className="md:grid md:grid-cols-[3fr,1fr] flex flex-col-reverse">
-          <div className="flex flex-1 h-24">
-            <Heading2>{title}</Heading2>
-          </div>
+    <CategoriesNavigation
+      question={question}
+      categoriesWithIndexes={categoriesWithIndexes}
+      questionsLength={MAX_QUESTION_NUMBER}
+    >
+      <div className="bg-white-100 p-6 rounded-md">
+        <div className="text-xs py-2">
+          Frage {categoriesWithIndexes?.[category].findIndex((element) => element === id) || 0 + 1}/
+          {categoriesWithIndexes?.[category].length}
         </div>
-        <InputField
-          type="number"
-          label={label}
-          name="answer"
-          id="number"
-          step="1"
-          placeholder={initialAnswer?.toString()}
-          min="0"
-          max="100"
-          value={answer}
-          onChange={(value) => {
-            setAnswer(value);
-            setImpact((parseFloat(value) * parseFloat(emissionfactor)).toString());
-          }}
-          onKeyDown={(key) => key === 'Enter' && router.push(hrefNextQuestion)}
-        />
-        <br />
-        {answer ? <Copy>Your impact is {impact}</Copy> : <Copy>Please answer the question</Copy>}
-      </div>
-      <div className="grid grid-cols-2 md:grid-cols-[2fr,1fr] items-center">
-        {parseInt(id) <= 1 ? (
-          <div />
-        ) : (
-          <div className="grid justify-start">
-            <LinkElement
-              href={`/rechner/${(parseInt(id) - 1).toString()}`}
-              onClick={() => saveCurrentQuestionIntoLocalStorage()}
-            >
-              <Button buttonColorGray={true}>← Vorherige Frage</Button>
-            </LinkElement>
+        <div>
+          <div className="md:grid md:grid-cols-[3fr,1fr] flex flex-col-reverse">
+            <div className="flex flex-1 h-24">
+              <Heading2>{title}</Heading2>
+            </div>
           </div>
-        )}
-        <LinkElement href={hrefNextQuestion} onClick={() => saveCurrentQuestionIntoLocalStorage()}>
-          <div className="flex justify-end">
-            <Button>{parseInt(id) >= MAX_QUESTION_NUMBER ? 'Save' : 'Nächste Frage →'}</Button>
-          </div>
-        </LinkElement>
+          <InputField
+            type="number"
+            label={label}
+            name="answer"
+            id="number"
+            step="1"
+            placeholder={initialAnswer?.toString()}
+            min="0"
+            max="100"
+            value={answer}
+            onChange={(value) => {
+              setAnswer(value);
+              setImpact((parseFloat(value) * parseFloat(emissionfactor)).toString());
+            }}
+            onKeyDown={(key) => key === 'Enter' && router.push(hrefNextQuestion)}
+          />
+          <br />
+        </div>
+        <div className="flex flex-wrap gap-4 items-center">
+          {parseInt(id) <= 1 ? (
+            <div />
+          ) : (
+            <div className="grid justify-start">
+              <LinkElement href={`/rechner/${(parseInt(id) - 1).toString()}`} onClick={saveCurrentQuestionIntoLocalStorage}>
+                <Button buttonColorGray={true}>Zurück</Button>
+              </LinkElement>
+            </div>
+          )}
+          <LinkElement href={hrefNextQuestion} onClick={saveCurrentQuestionIntoLocalStorage}>
+            <div className="flex justify-end">
+              <Button>{parseInt(id) >= MAX_QUESTION_NUMBER ? 'Save' : 'Weiter'}</Button>
+            </div>
+          </LinkElement>
+        </div>
       </div>
-      <div className="border-2 p-8 mt-24 bg-gray-300">
+      <div className="mt-24">
         <div className="font-bold py-2 flex flex-row">
           <InformationCircleIcon className="h-6 w-6 mr-2" />
           What is happening here?
@@ -135,13 +129,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
   const currentId = params?.frage || '1';
   const questions = await getQuestions();
-
-  // eslint-disable-next-line no-constant-condition
-  if (false) {
-    setQuestions();
-  }
-
-  const allCategoriesWithIndexes = questions.reduce((acc, obj) => {
+  const question = questions.find(({ id }) => currentId === id) || questions[0];
+  const categoriesWithIndexes = questions.reduce((acc, obj) => {
     const key = obj['category'];
     if (!acc[key]) {
       acc[key] = [];
@@ -152,35 +141,13 @@ export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
 
   return {
     props: {
-      allCategoriesWithIndexes,
-      question: questions.find(({ id }) => currentId === id) || questions[0],
+      questions,
+      categoriesWithIndexes,
+      question,
       MAX_QUESTION_NUMBER: questions.length,
     },
     revalidate: 14400,
   };
-};
-
-const getQuestions = async () => {
-  const database = await initializeFirestore();
-
-  const q = query(collection(database, 'questions'));
-
-  const querySnapshot = await getDocs(q);
-  const questions: Question[] = [];
-  querySnapshot.forEach((doc) => {
-    questions.push(doc.data() as Question);
-  });
-  return questions;
-};
-
-const setQuestions = async () => {
-  const database = await initializeFirestore();
-  const questions = Questions;
-
-  questions.map(
-    async ({ id, title, category, label, initialAnswer, emissionfactor }) =>
-      await setDoc(doc(database, 'questions', id), { id, initialAnswer, label, emissionfactor, category, title })
-  );
 };
 
 export default Frage;
