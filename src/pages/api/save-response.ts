531a91e4784b58bc486a82349348bff3673ea0c1
Céline <42832501+celine-s@users.noreply.github.com>
2021-12-20 11:08:50 +0100

    feat: save responses firestore (#11)
    * feat: save responses firestore
    
    * fb mirco
    
    * remove GET
    
    Co-authored-by: Celine Salzmann <celine@smartive.ch>

---
+++ src/pages/api/save-response.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { initializeFirestore } from '../../utils/get-firestore';
+import { doc, setDoc } from 'firebase/firestore/lite';
+
+type Data = {
+  id: number;
+  answers: string[];
+};
+
+export default async function saveResponses(req: NextApiRequest, res: NextApiResponse<Data | Error>) {
+  const db = await initializeFirestore();
+  if (req.method === 'POST') {
+    try {
+      await setDoc(doc(db, 'responses', '/' + req.body.id + '/'), { id: req.body.id, answers: req.body.answers });
+      return res.status(200).send({ id: req.body.id, answers: req.body.answers });
+    } catch (error) {
+      return res.status(500).send(error as Error);
+    }
+  } else {
+    return res.status(405);
+  }
+}
--- src/pages/rechner/report.tsx
+++ src/pages/rechner/report.tsx
@@ -1,11 +1,10 @@
 import { NextPage } from 'next';
 import { Page } from '../../layouts/page';
 import { Heading2 } from '../../identity/heading-2';
-import { LinkElement } from '../../elements/link';
 import { InputField } from '../../elements/input-field';
 import React from 'react';
 
-const Saved: NextPage = () => {
+const Report: NextPage = () => {
   return (
     <Page>
       <Heading2>You successfully saved all data.</Heading2>
@@ -17,14 +16,9 @@ const Saved: NextPage = () => {
           label="Send me the report"
           placeholder="franz@gmail.com"
         ></InputField>
-        <div className="my-4 flex items-center">
-          <LinkElement href="/rechner/1" border={true}>
-            Back to Calculator
-          </LinkElement>
-        </div>
       </div>
     </Page>
   );
 };
 
-export default Saved;
+export default Report;
