788e5c4bec949dac00725d9134bd593e881f8016
Celine Salzmann <celine@smartive.ch>
2021-12-20 09:11:09 +0100

    remove GET

--- src/pages/api/save-response.ts
+++ src/pages/api/save-response.ts
@@ -7,21 +7,16 @@ type Data = {
   answers: string[];
 };
 
-export default async function saveResponses(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function saveResponses(req: NextApiRequest, res: NextApiResponse<Data | Error>) {
   const db = await initializeFirestore();
-
-  if (req.method === 'GET') {
-    res.status(200).json({ id: Date.now(), answers: ['23', 'get responses was called'] });
-    console.log('get responses was called');
-  } else if (req.method === 'POST') {
+  if (req.method === 'POST') {
     try {
       await setDoc(doc(db, 'responses', '/' + req.body.id + '/'), { id: req.body.id, answers: req.body.answers });
       return res.status(200).send({ id: req.body.id, answers: req.body.answers });
     } catch (error) {
-      console.log(error);
-      //return res.status(500).send(error);
+      return res.status(500).send(error as Error);
     }
   } else {
-    // Handle any other HTTP method
+    return res.status(405);
   }
 }
