c0a57491a37e357397347b2fa5b9657c9b9e73e2
Celine Salzmann <celine@smartive.ch>
2021-12-30 18:10:09 +0100

    renaming selected => answer

--- src/compositions/question-form-5.tsx
+++ src/compositions/question-form-5.tsx
@@ -4,16 +4,16 @@ import { DropDown } from '../elements/dropdown';
 const heatingTypes = ['Heizöl', 'Erdgas', 'Strom', 'Holz', 'Wärmepumpe', 'Fernwärme', 'Solarenergie', 'Weiss nicht'];
 
 type Props = {
-  selected: { [key: string]: string };
+  answer: { [key: string]: string };
   callback: React.Dispatch<React.SetStateAction<{ [key: string]: string }>>;
 };
 
-export const Question5: FC<Props> = ({ selected, callback }) => {
+export const Question5: FC<Props> = ({ answer, callback }) => {
   const initialAnswer = { heatingType: 'Heizöl' };
   useEffect(() => {
     callback(initialAnswer);
   }, []);
   return (
-    <DropDown selection={heatingTypes} selected={selected || initialAnswer} callback={callback} optionKey={'heatingType'} />
+    <DropDown selection={heatingTypes} selected={answer || initialAnswer} callback={callback} optionKey={'heatingType'} />
   );
 };
--- src/compositions/question-form-6.tsx
+++ src/compositions/question-form-6.tsx
@@ -4,16 +4,16 @@ import { DropDown } from '../elements/dropdown';
 const years = ['vor 1980', '1981-1990', '1991-2000', '2001-2010', '2011-heute', 'Weiss nicht'];
 
 type Props = {
-  selected: { [key: string]: string };
+  answer: { [key: string]: string };
   callback: React.Dispatch<React.SetStateAction<{ [key: string]: string }>>;
 };
 
-export const Question6: FC<Props> = ({ selected, callback }) => {
+export const Question6: FC<Props> = ({ answer, callback }) => {
   const initialAnswer = { constructionPeriod: '2011-heute' };
   useEffect(() => {
     callback(initialAnswer);
   }, []);
   return (
-    <DropDown selection={years} selected={selected || initialAnswer} callback={callback} optionKey="constructionPeriod" />
+    <DropDown selection={years} selected={answer || initialAnswer} callback={callback} optionKey="constructionPeriod" />
   );
 };
