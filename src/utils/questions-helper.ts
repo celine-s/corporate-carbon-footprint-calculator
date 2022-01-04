1dd4325ba46f216389258607ffaa31a600cbd999
Celine Salzmann <celine@smartive.ch>
2022-01-04 12:46:30 +0100

    delete setQuestions()

--- src/utils/questions-helper.ts
+++ src/utils/questions-helper.ts
@@ -1,7 +1,6 @@
-import { collection, doc, getDocs, query, setDoc } from 'firebase/firestore/lite';
+import { collection, getDocs, query } from 'firebase/firestore/lite';
 import { Question } from '../data/question';
 import { initializeFirestore } from './get-firestore';
-import Questions from '../data/questions.json';
 
 export const getQuestions = async () => {
   const database = await initializeFirestore();
@@ -14,30 +13,4 @@ export const getQuestions = async () => {
     questions.push(doc.data() as Question);
   });
   return questions;
-};
-
-export const setQuestions = async () => {
-  const database = await initializeFirestore();
-  const questions = Questions;
-
-  try {
-    for (const idx in questions) {
-      const { id, title, category, label, infobox, initialAnswer, whatText, whatTitle, constraints, emissionfactor } =
-        questions[idx];
-      await setDoc(doc(database, 'questions', id), {
-        id,
-        title,
-        category,
-        label,
-        infobox,
-        initialAnswer,
-        whatText,
-        whatTitle,
-        constraints,
-        emissionfactor,
-      });
-    }
-  } catch (error) {
-    console.log(error);
-  }
 };
